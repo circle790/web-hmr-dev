cf90426470628129803f0d711f213848f1f89612
circle790 <circle790@163.com>
2020-08-06 15:33:30 +0800

    修改，测试

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,16 @@ $(function () {
    * 详细页
    */
   $(document).on("pageInit", ".page-detail", function (e, id, page) {
+    $("#picker").picker({
+      rotateEffect: true,
+      toolbarCloseText: '2222',
+      cols: [
+        {
+          textAlign: 'center',
+          values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+        }
+      ]
+    });
     $.toast("这是详细页");
   });
   /**
